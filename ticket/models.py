b9aac618275beea4bb71754210cda35e9f1fbce8
chukby <56086064+chukby@users.noreply.github.com>
2023-10-17 11:05:53 +0000

    Add ticket model

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,35 @@
+import uuid
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Ticket(models.Model):
+    TICKET_STATE = (
+        ('open', 'Open'),
+        ('closed', 'Closed'),
+        ('pending', 'Pending'),
+    )
+    ticket_number = models.UUIDField(default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.CASCADE,
+                                   related_name='created_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    assigned_to = models.ForeignKey(User,
+                                    on_delete=models.DO_NOTHING,
+                                    null=True,
+                                    blank=True)
+    date_accepted = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    is_resolved = models.BooleanField(default=False)
+    date_closed = models.DateTimeField(null=True, blank=True)
+    closed_by = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='closed_by')
+    ticket_state = models.CharField(max_length=12, choices=TICKET_STATE)
+
+    def __str__(self):
+        return self.title
