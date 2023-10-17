3e58ef0108c0dee05349b98132a00d902f055f26
chukby <56086064+chukby@users.noreply.github.com>
2023-10-17 04:18:23 +0000

    Require login to access the dashboard view
    Use `@login_required` decorator to restrict access to the dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from djang0.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# Create dashboard
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
